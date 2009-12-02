c2bcbf69050276d51879d0328f1fe08d0579b485
Kevin Marsh <kevin.marsh@gmail.com>
2009-12-02 16:22:01 -0500

    Hook into controller and get query

--- lib/estimated_time_sidebar_hook.rb
+++ lib/estimated_time_sidebar_hook.rb
@@ -2,7 +2,19 @@
 class EstimatedTimeSidebarHook < Redmine::Hook::ViewListener
 
   def view_layouts_base_sidebar(context = { })
-    %Q(Time: #{Time.now})
+    query = context[:controller].instance_variable_get("@query") rescue nil
+    
+    if query
+      if query.grouped?
+        response = query.issue_count_by_group
+        %Q(<h3>Estimated time</h3><pre>#{response.inspect}</pre>)
+      else
+        response = query.issues.size
+        %Q(<h3>Estimated time</h3><pre>#{response.inspect} issues</pre>)        
+      end 
+    else
+        %Q(<h3>Estimated time</h3><pre>No query</pre>)      
+    end
   end
 
 end
