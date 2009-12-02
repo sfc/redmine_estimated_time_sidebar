b88609e463fd5f3a4c10578d5ba3a7a7ad6b2a81
Kevin Marsh <kevin.marsh@gmail.com>
2009-12-02 17:02:37 -0500

    Start grouping

--- lib/estimated_time_sidebar_hook.rb
+++ lib/estimated_time_sidebar_hook.rb
@@ -2,14 +2,27 @@
 class EstimatedTimeSidebarHook < Redmine::Hook::ViewListener
 
   def view_layouts_base_sidebar(context = { })
+    output = ''
+    output << '<h3>Total Estimated time</h3>'
+    
     query = context[:controller].instance_variable_get("@query") rescue nil
     
     if query
-      total_estimated_time = query.issue_sum
-      %Q(<h3>Total Estimated time</h3>#{html_hours("%.2f" % total_estimated_time)})
+      if query.grouped?
+        total_estimated_time_by_group = query.issue_sum_estimated_hours_by_group
+        
+        total_estimated_time_by_group.each do |group, estimated_hours|
+          name = group.name rescue group.id
+          output << %Q(#{name}: #{'%.2f' % estimated_hours}<br />)
+        end        
+      else
+        total_estimated_time = query.issue_sum_estimated_hours
+        output << %Q(#{html_hours("%.2f" % total_estimated_time)})        
+      end
     else
-        %Q(<h3>Estimated time</h3><pre>No query</pre>)      
+      %Q(<pre>No query</pre>)      
     end
+    
+    output
   end
-
 end
--- lib/query_patch.rb
+++ lib/query_patch.rb
@@ -3,14 +3,11 @@ require_dependency 'query'
 module QueryPatch
   def self.included(base) # :nodoc:
     base.extend(ClassMethods)
- 
-    base.send(:include, InstanceMethods)
- 
-    # Same as typing in the class 
+    base.send(:include, InstanceMethods) 
+
     base.class_eval do
-      unloadable # Send unloadable so it will not be unloaded in development
+      unloadable
     end
- 
   end
   
   module ClassMethods
@@ -18,12 +15,31 @@ module QueryPatch
   end
   
   module InstanceMethods
-    def issue_sum
+    def issue_sum_estimated_hours
       Issue.sum(:estimated_hours, :include => [:status, :project], :conditions => statement)
     rescue ::ActiveRecord::StatementInvalid => e
       raise StatementInvalid.new(e.message)
     end
-  end
+
+    def issue_sum_estimated_hours_by_group
+      r = nil
+      if grouped?
+        begin
+          # Rails will raise an (unexpected) RecordNotFound if there's only a nil group value
+          r = Issue.sum(:estimated_hours, :group => group_by_statement, :include => [:status, :project], :conditions => statement)
+        rescue ActiveRecord::RecordNotFound
+          r = { nil => issue_count }
+        end
+        c = group_by_column
+        if c.is_a?(QueryCustomFieldColumn)
+          r = r.keys.inject({}) {|h, k| h[c.custom_field.cast_value(k)] = r[k]; h}
+        end
+      end
+      r
+    rescue ::ActiveRecord::StatementInvalid => e
+      raise StatementInvalid.new(e.message)
+    end
+  end  
 end
  
 # Add module to Query
