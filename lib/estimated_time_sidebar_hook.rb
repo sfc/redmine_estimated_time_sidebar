4ef6d6627177dd4d2dee1b5738ed786bb6e58cde
Kevin Marsh <kevin.marsh@gmail.com>
2009-12-03 16:32:48 -0500

    Don't show if there is no query available, i.e., on issue pages

--- lib/estimated_time_sidebar_hook.rb
+++ lib/estimated_time_sidebar_hook.rb
@@ -3,12 +3,12 @@ class EstimatedTimeSidebarHook < Redmine::Hook::ViewListener
 
   def view_layouts_base_sidebar(context = { })
     query = context[:controller].instance_variable_get("@query") rescue nil
-    
+
     if query
       total_estimated_time = query.issue_sum
       %Q(<h3>Total Estimated time</h3>#{html_hours("%.2f" % total_estimated_time)})
     else
-        %Q(<h3>Estimated time</h3><pre>No query</pre>)      
+      ''
     end
   end
 
--- lib/query_patch.rb
+++ lib/query_patch.rb
@@ -1,22 +1,22 @@
 require_dependency 'query'
- 
+
 module QueryPatch
   def self.included(base) # :nodoc:
     base.extend(ClassMethods)
- 
+
     base.send(:include, InstanceMethods)
- 
-    # Same as typing in the class 
+
+    # Same as typing in the class
     base.class_eval do
       unloadable # Send unloadable so it will not be unloaded in development
     end
- 
+
   end
-  
+
   module ClassMethods
-    
+
   end
-  
+
   module InstanceMethods
     def issue_sum
       Issue.sum(:estimated_hours, :include => [:status, :project], :conditions => statement)
@@ -25,6 +25,6 @@ module QueryPatch
     end
   end
 end
- 
+
 # Add module to Query
 Query.send(:include, QueryPatch)
