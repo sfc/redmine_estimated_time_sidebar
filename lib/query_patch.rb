f6480c819d9110d77785a2ac7e9734be7044a830
Kevin Marsh <kevin.marsh@gmail.com>
2010-08-27 10:29:21 -0400

    Fix for counting child tests + parent tasks

--- lib/query_patch.rb
+++ lib/query_patch.rb
@@ -19,13 +19,13 @@ module QueryPatch
 
   module InstanceMethods
     def issue_sum
-      Issue.sum(:estimated_hours, :include => [:status, :project], :conditions => statement)
+      Issue.sum('IF(parent_id IS NULL, estimated_hours, 0)', :include => [:status, :project], :conditions => statement)
     rescue ::ActiveRecord::StatementInvalid => e
       raise StatementInvalid.new(e.message)
     end
 
     def issue_sum_remaining
-      Issue.sum('estimated_hours * (100 - (done_ratio))/100', :include => [:status, :project], :conditions => statement)
+      Issue.sum('IF(parent_id IS NULL, estimated_hours, 0) * (100 - (done_ratio))/100', :include => [:status, :project], :conditions => statement)
     rescue ::ActiveRecord::StatementInvalid => e
       raise StatementInvalid.new(e.message)
     end
